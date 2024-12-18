b358ee5cec8ef67b4c6e0aa979d3bb3f432e2ea8
Ethan Weir <Weircode@gmail.com>
2024-12-18 14:43:45 -0800

    faster?

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ function createSnow(){
             snowflakeCount--;
         }, (speed+5) * 1000);
     }
-    setInterval(createNewSnowflake, 200);
+    setInterval(createNewSnowflake, 100);
 }
 function createAurora(){
 
